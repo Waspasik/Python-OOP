1a46ba35fea0a61f53273691b4a4168bcf7cef4c
Maksim Kisel <103454376+Waspasik@users.noreply.github.com>
2023-07-05 23:35:59 +0300

    Added missing task "Класс SkipIterator"

--- Protocol of iterables and iterators.py
+++ Protocol of iterables and iterators.py
@@ -66,3 +66,37 @@ class DevelopmentTeam:
     
     def __iter__(self):
         yield from self._junior + self._senior
+
+
+
+# Класс SkipIterator
+# Реализуйте класс SkipIterator. При создании экземпляра класс должен принимать два аргумента в
+# следующем порядке:
+
+# iterable — итерируемый объект
+# n — целое неотрицательное число
+
+# Экземпляр класса SkipIterator должен являться итератором, который генерирует элементы итерируемого
+# объекта iterable, пропуская по n элементов, а затем возбуждает исключение StopIteration.
+
+# Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что
+# реализованный класс используется только с корректными данными.
+
+# Примечание 2. Класс SkipIterator должен удовлетворять протоколу итератора, то есть иметь методы
+# __iter__() и __next__(). Реализация же протокола может быть произвольной.
+
+class SkipIterator:
+    def __init__(self, iterable, n):
+        self.iterable = list(iterable)
+        self.n = n
+        self._index = -n-1
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self._index += self.n+1
+        if self._index >= len(self.iterable):
+            raise StopIteration
+        else:
+            return self.iterable[self._index]
